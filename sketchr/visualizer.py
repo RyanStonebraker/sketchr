3bd8cdce398d94200347300b0347e4741e0b3f25
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-22 12:49:09 -0800

    Adds custom image adding and styling props

--- sketchr/visualizer.py
+++ sketchr/visualizer.py
@@ -1,10 +1,11 @@
 import classifier
 import cbir
+import uuid
 
 class Visualizer():
     def __init__(self, scene):
         self.scene = scene
-        # self.cbir = cbir.CBIR()
+        self.cbir = cbir.CBIR()
         self.dom = self.initDom()
 
     def initDom(self):
@@ -38,15 +39,44 @@ class Visualizer():
                 html += self.getStructure(structureHead[head])
         return html
 
+    def queryDom(self, query):
+        if not query:
+            return self.dom
+
+        query = query.split(" ")
+        element = self.dom
+        for el in query:
+            element = element["inner"][el]
+        return element
+
+    def getStyleString(self, style):
+        output = "style='position:absolute;"
+        for name, prop in style.items():
+            output += "{0}:{1};".format(name, prop)
+        output += "'"
+        return output
+
+    def addImage(self, src, pos, style={}):
+        id = uuid.uuid4()
+        props = "src='{}'".format(src)
+        style["top"] = pos["x"]
+        style["left"] = pos["y"]
+        style["z-index"] = pos["z"]
+        props += self.getStyleString(style)
+        self.queryDom("html body")["inner"][id] = self.createElement("img", props)
+
     def generate(self):
-        # for background in self.scene["backgrounds"]:
-            # backgroundImage = self.cbir.retrieveImage(background["subject"])
+        for i, background in enumerate(self.scene["backgrounds"]):
+            backgroundImage = self.cbir.retrieveImage(background["subject"])
+            self.addImage(backgroundImage, {"x": 0, "y": 0, "z": 0})
+            # self.queryDom("html body")["inner"]["background" + str(i)] = self.createElement("img", "src='{}'".format(backgroundImage))
+        for i, object in enumerate(self.scene["objects"]):
+            objectImage = self.cbir.retrieveImage(object["subject"])
         print(self.getStructure(self.dom))
 
 if __name__ == "__main__":
-    # classifyEngine = classifier.Classifier()
-    # query = "A yellow dog is chasing a car in Anchorage. A red dog is walking. The red dog is humungous. 2 dogs ran."
-    # output = classifyEngine.classify(query)
-    output = "Test"
+    classifyEngine = classifier.Classifier()
+    query = "A yellow dog is chasing a car in Anchorage. A red dog is walking. The red dog is humungous. 2 dogs ran."
+    output = classifyEngine.classify(query)
     visualEngine = Visualizer(output)
     visualEngine.generate()

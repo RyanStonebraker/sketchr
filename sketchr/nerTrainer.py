22bf7a5379ae23fa409477fdea18318a02ed3d3c
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-23 15:35:26 -0800

    Adds NER location model

--- sketchr/nerTrainer.py
+++ sketchr/nerTrainer.py
@@ -23,11 +23,8 @@ def getTrainingData(dataFile="corpora/nerTrainingData.txt", locFile="corpora/loc
             trainingData.append(trainingSentence)
     return trainingData
 
-def train(trainingData, model=None, output_dir=None, n_iter=100):
-    if model:
-        nlp = spacy.load(model)
-    else:
-        nlp = spacy.blank("en")
+def train(trainingData, model=None, outputDir=None, trainingIteration=100, testDataFile="corpora/unlabeledData.txt"):
+    nlp = spacy.load(model) if model else spacy.blank("en")
 
     if "ner" not in nlp.pipe_names:
         ner = nlp.create_pipe("ner")
@@ -43,7 +40,7 @@ def train(trainingData, model=None, output_dir=None, n_iter=100):
     with nlp.disable_pipes(*other_pipes):
         if model is None:
             nlp.begin_training()
-        for itn in range(n_iter):
+        for itn in range(trainingIteration):
             random.shuffle(trainingData)
             losses = {}
             batches = minibatch(trainingData, size=compounding(4.0, 32.0, 1.001))
@@ -52,26 +49,20 @@ def train(trainingData, model=None, output_dir=None, n_iter=100):
                 nlp.update(texts, annotations, drop=0.5, losses=losses)
             print("Losses", losses)
 
-    for text, _ in trainingData:
-        doc = nlp(text)
-        print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-        print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+    if outputDir:
+        outputDir = Path(outputDir)
+        if not outputDir.exists():
+            outputDir.mkdir()
+        nlp.to_disk(outputDir)
 
-    if output_dir:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
-        nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)
-
-        print("Loading from", output_dir)
-        nlp2 = spacy.load(output_dir)
-        for text, _ in trainingData:
-            doc = nlp2(text)
-            print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
-            print("Tokens", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
+    with open(testDataFile, "r") as testReader:
+        for sentence in testReader:
+            sentence = sentence.strip()
+            doc = nlp(sentence)
+            print("Entities:", [(ent.text, ent.label_) for ent in doc.ents])
+            print("Tokens:", [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
 
 if __name__ == "__main__":
-    print(getTrainingData())
-    # train(trainingData)
+    trainingData = getTrainingData()
+    train(trainingData, outputDir="models/nerModel")

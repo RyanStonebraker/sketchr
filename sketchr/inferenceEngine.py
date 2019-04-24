f250c1c3ebdcfa87804f4065ff7d37b66dffdd9d
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-04-24 12:05:43 -0800

    Inference engine returns sorted list of describing words
    If number of words is specifed, they are chosen based on their normalized frequency probability

---
+++ sketchr/inferenceEngine.py
@@ -0,0 +1,77 @@
+import spacy
+import random
+from collections import OrderedDict
+
+class InferenceEngine():
+    def __init__(self, *corpora):
+        self.nlp = spacy.load("en")
+        self.descriptions = {}
+        for corpusFile in corpora:
+            with open(corpusFile, "r") as corpusReader:
+                paragraph = ""
+                count = 0
+                for line in corpusReader:
+                    line = line.lower().strip()
+                    if line:
+                        paragraph += " {}".format(line)
+                    else:
+                        self.classifyParagraph(paragraph)
+                        paragraph = ""
+
+                    count += 1
+                    if count > 1000:
+                        break
+
+    def classifyParagraph(self, paragraph):
+        doc = self.nlp(paragraph)
+        for chunk in doc.noun_chunks:
+            adjBuffer = []
+            for word in chunk:
+                if word.pos_ == "ADJ":
+                    adjBuffer.append(word.text)
+                elif word.pos_ == "NOUN":
+                    self.addDescriptors(word.lemma_, adjBuffer)
+                    adjBuffer = []
+
+    def addDescriptors(self, noun, adjectives):
+        for adjective in adjectives:
+            if noun in self.descriptions:
+                if adjective in self.descriptions[noun]:
+                    self.descriptions[noun][adjective] += 1
+                else:
+                    self.descriptions[noun][adjective] = 1
+            else:
+                self.descriptions[noun] = {}
+                self.descriptions[noun][adjective] = 1
+
+    def getDescriptiveWords(self, word, numberWords=None):
+        if word not in self.descriptions:
+            return []
+
+        describingWords = self.descriptions[word]
+        describingWords = OrderedDict(sorted(describingWords.items(), key=lambda w: w[1], reverse=True))
+
+        if numberWords is None:
+            numberWords = len(describingWords)
+
+        totalAdjectiveCount = 0
+        for _, count in describingWords.items():
+            totalAdjectiveCount += count
+
+        descriptions = []
+        for i, adjCount in enumerate(describingWords.items()):
+            adjective, count = adjCount
+            if len(descriptions) >= numberWords:
+                break
+
+            normalizedFreq = count/totalAdjectiveCount
+            remainingAdjectives = len(describingWords) - i
+            if (numberWords and remainingAdjectives <= numberWords - len(descriptions)) or random.random() < normalizedFreq:
+                descriptions.append(adjective)
+        return descriptions
+
+if __name__ == "__main__":
+    inferenceEngine = InferenceEngine("corpora/mobydick.txt")
+    while True:
+        user = input("Enter Word: ")
+        print(inferenceEngine.getDescriptiveWords(user))
